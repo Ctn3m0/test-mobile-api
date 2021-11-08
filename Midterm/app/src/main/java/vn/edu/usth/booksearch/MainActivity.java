6aa57ed6bdd62e5369827407631c06ca77cf493c
ctn3m0 <hieuquang15801@gmail.com>
2021-11-08 13:06:11 +0700

    get the image but :))

--- Midterm/app/src/main/java/vn/edu/usth/booksearch/MainActivity.java
+++ Midterm/app/src/main/java/vn/edu/usth/booksearch/MainActivity.java
@@ -6,7 +6,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import vn.edu.usth.booksearch.net.BookClient;
 import vn.edu.usth.booksearch.models.Book;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Message;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -15,6 +20,11 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import android.util.Log;
@@ -40,7 +50,9 @@ public class MainActivity extends AppCompatActivity {
     private boolean search;
     private ImageButton Search;
     private EditText input;
-
+    Bitmap bitmap;
+    private ImageView test;
+    URL url;
 
 
     @Override
@@ -54,18 +66,77 @@ public class MainActivity extends AppCompatActivity {
         mTextViewResult.setText("123213");
 
         search = false;
-
+        test = findViewById(R.id.test_image);
 
         input = (EditText) findViewById(R.id.search_input);
         Search = (ImageButton) findViewById(R.id.search_button);
+
+        AsyncTask<String, Integer, Message> task = new AsyncTask<String, Integer, Message>() {
+            @Override
+            protected void onPreExecute() {
+                // do some preparation here, if needed
+            }
+            @Override
+            protected Message doInBackground(String... params) {
+                try {
+                    // wait for 5 seconds to simulate a long network access
+//                    Thread.sleep(5000);
+//                  initialize URL
+                    url = new URL("https://images.unsplash.com/photo-1615789591457-74a63395c990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80");
+                    // Make a request to server
+                    HttpURLConnection connection =
+                            (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setDoInput(true);
+                    // allow reading response code and response dataconnection.
+                    connection.connect();
+                    // Receive response
+                    int response = connection.getResponseCode();
+                    Log.i("USTHWeather", "The response is: " + response);
+                    InputStream is = connection.getInputStream();
+                    // Process image response
+                    bitmap = BitmapFactory.decodeStream(is);
+                    connection.disconnect();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+                // Assume that we got our data from server
+                Bundle bundle = new Bundle();
+                bundle.putString("server_response", "some sample json here");
+                // notify main thread
+                Message msg = new Message();
+                msg.setData(bundle);
+                return msg;
+            }
+            @Override
+            protected void onProgressUpdate(Integer... values) {
+                // This method is called in the main thread, so it's possible
+                // to update UI to reflect the worker thread progress here.
+                // In a network access task, this should update a progress bar
+                // to reflect how many percent of data has been retrieved
+//                logo = (ImageView) findViewById(R.id.logo);
+//                logo.setImageBitmap(bitmap);
+
+            }
+            @Override
+            protected void onPostExecute(Message msg) {
+//                test.setImageDrawable(new BitmapDrawable(bitmap));
+                test.setImageResource(R.drawable.cloud);
+                test.setImageDrawable(new BitmapDrawable(bitmap));
+                String content = msg.getData().getString("server_response");
+                Toast.makeText(MainActivity.this, content, Toast.LENGTH_SHORT).show();
+            }
+        };
+
         Search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                task.execute();
                 fetchBooks(input.getText().toString(), search);
                 Toast.makeText(MainActivity.this, input.getText().toString(), Toast.LENGTH_LONG).show();
             }
         });
-
 
     }
 
@@ -90,6 +161,7 @@ public class MainActivity extends AppCompatActivity {
 
                         for (Book book : books) {
                             books_info.add(book.getTitle()+" \n"+book.getAuthor());
+//                            books_info.add(book.getCoverUrl());
                             Log.i("Data",book.getTitle());
                             Log.i("Image",book.getCoverUrl());
                             mTextViewResult.setText(book.toString());
@@ -107,6 +179,7 @@ public class MainActivity extends AppCompatActivity {
                 Log.v("BIG PROBLEM", "FAIL");
             }
         });
+
     }
 
 }
